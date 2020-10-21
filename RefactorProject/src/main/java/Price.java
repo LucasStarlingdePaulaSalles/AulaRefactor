120280f1e9c3f16d0b0139aed388b612bfdca0c6
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-10-20 22:41:25 -0300

    Commit 15

--- RefactorProject/src/main/java/Price.java
+++ RefactorProject/src/main/java/Price.java
@@ -2,9 +2,6 @@ public abstract class Price {
    public abstract int getPriceCode();
    public abstract double getCharge(int daysRented);
    public int getFrequentRenterPoints(int daysRented) {
-      int number = 1;
-         if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) number ++;
-      return number;
+       return 1;
    }
 }
