decc8e3ba006df6d9ceff697ffa59f6b92d85cdd
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2020-10-20 23:20:10 -0300

    Commit 17

--- RefactorProject/src/main/java/HtmlStatement.java
+++ RefactorProject/src/main/java/HtmlStatement.java
@@ -1,21 +1,39 @@
 import java.util.Enumeration;
+
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
-      }
-      //add footer lines
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
-      return result;
-   }
+  private String getHeaderData(Customer aCustomer) { 
+    return "<H1>Rentals for <EM>" + 
+            aCustomer.getName() + 
+            "</EM></H1><P>\n";
+  }
+
+  private String getDataPerRental(Rental aRental) { 
+    return aRental.getMovie().getTitle()+
+            ": " +
+            String.valueOf(aRental.getCharge()) + 
+            "<BR>\n";
+  }
+
+  private String getFooterData(Customer aCustomer) {
+    return "<P>You owe <EM>" +
+            String.valueOf(aCustomer.getTotalCharge()) + 
+            "</EM><P>\n" +
+            "On this rental you earned <EM>" + 
+            String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
+  }
+
+  public String value(Customer aCustomer) {
+    Enumeration rentals = aCustomer.getRentals();
+    String result = getHeaderData(aCustomer);
+    
+    while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        result += getDataPerRental(each);
+    }
+
+    result += getFooterData(aCustomer);
+
+    return result;
+  }
 }
--- RefactorProject/src/main/java/TextStatement.java
+++ RefactorProject/src/main/java/TextStatement.java
@@ -1,21 +1,40 @@
 import java.util.Enumeration;
 public class TextStatement extends Statement {
+    
+    private String getHeaderData(Customer aCustomer) { 
+        return "Rental Record for " + 
+                aCustomer.getName() +
+                "\n";
+    }
+
+    private String getDataPerRental(Rental aRental) { 
+        return "\t" + 
+                aRental.getMovie().getTitle()+ 
+                "\t" +
+                String.valueOf(aRental.getCharge()) + 
+                "\n";
+    }
+
+    private String getFooterData(Customer aCustomer) {
+        return "Amount owed is " +
+                String.valueOf(aCustomer.getTotalCharge()) + 
+                "\n" +
+                "You earned " +
+                String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+                " frequent renter points";
+    }
+    
     public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
-      }
-      //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
-      return result;
-   }
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeaderData(aCustomer);
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += getDataPerRental(each);
+        }
+
+        result += getFooterData(aCustomer);
+
+        return result;
+    }
 }
